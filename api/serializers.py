23b53fe2087ef3ffb4171ee0d4929c946d33e297
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-02-19 03:36:13 +0500

    commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from api.models import Book
 from django.contrib.auth.models import User
+from rest_framework.authtoken.views import Token
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,5 +21,6 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
         return user
 
--- api/views.py
+++ api/views.py
@@ -3,6 +3,8 @@ from rest_framework import status, viewsets
 from api.models import Book
 from api.serializers import BookSerializer, UserSerializer
 from django.contrib.auth.models import User
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
@@ -10,6 +12,9 @@ from django.contrib.auth.models import User
 class BookView(viewsets.ModelViewSet):
     queryset=Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes=[TokenAuthentication]
+    permission_classes =[IsAuthenticated]
+    
 
 class UserView(viewsets.ModelViewSet):
     queryset=User.objects.all()
