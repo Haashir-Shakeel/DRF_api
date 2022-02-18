062ef3f596c17b0cdba89690ab8ad6a5ee42c8b3
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-02-19 03:12:08 +0500

    commit

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path,include
 
-from api.views import BookView
+from api.views import BookView, UserView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('book',BookView,basename='bookview')
+router.register('users',UserView)
 
 urlpatterns=[
     path('api/',include(router.urls)),
-    path('api/<int:pk>/',include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import status, viewsets
 from api.models import Book
-from api.serializers import BookSerializer
-
+from api.serializers import BookSerializer, UserSerializer
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -10,3 +10,7 @@ from api.serializers import BookSerializer
 class BookView(viewsets.ModelViewSet):
     queryset=Book.objects.all()
     serializer_class = BookSerializer
+
+class UserView(viewsets.ModelViewSet):
+    queryset=User.objects.all()
+    serializer_class = UserSerializer
